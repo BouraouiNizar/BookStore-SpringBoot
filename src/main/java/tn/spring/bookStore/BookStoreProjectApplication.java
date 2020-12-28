e90fef86396dfc6508723862913df05c8a6edb2a
BouraouiNizar <bouraoui.nizar@esprit.tn>
2020-12-28 15:09:26 +0100

    add jpa hibernate configuration

--- src/main/java/tn/spring/bookStore/BookStoreProjectApplication.java
+++ src/main/java/tn/spring/bookStore/BookStoreProjectApplication.java
@@ -1,24 +1,10 @@
 package tn.spring.bookStore;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @SpringBootApplication
-public class BookStoreProjectApplication implements CommandLineRunner {
-	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	public static void main(String[] args) {
-		SpringApplication.run(BookStoreProjectApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
+public class BookStoreProjectApplication  {
 }
