025013b21aaf93a8af7548dfcd9091c14b2fc889
BouraouiNizar <bouraoui.nizar@esprit.tn>
2020-12-28 15:22:06 +0100

    add package entity add class command

--- src/main/java/tn/spring/bookStore/BookStoreProjectApplication.java
+++ src/main/java/tn/spring/bookStore/BookStoreProjectApplication.java
@@ -1,10 +1,18 @@
 package tn.spring.bookStore;
 
 
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class BookStoreProjectApplication  {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreProjectApplication.class, args);
+	}
+
+	
+
 }
